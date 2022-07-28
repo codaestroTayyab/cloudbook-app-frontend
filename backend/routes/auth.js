66a284c7357de7c8f876af2d9763e8aea136b4a1
codaestroTayyab <tayyab.saeed1@gmail.com>
2022-07-28 22:47:24 +0500

    backend added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+const fetchUser = require("../middleware/fetchUser");
 
 const router = express.Router();
+const AUTH_SECRET = "$Cloudbook$Mern";
 
-// POST METHOD for /cloudbook/auth/createuser. No login required
+//ENDPOINT 1: Post method for /cloudbook/auth/createuser. No login required
 router.post(
   "/createuser",
   [
@@ -26,21 +30,99 @@ router.post(
       if (check) {
         return res.status(400).json({ error: "Error! Email exist already" });
       }
+      //Adding Salt to password and hashing it using bcyrptJS
+      const saltPass = await bcrypt.genSalt(10);
+      const securedPass = await bcrypt.hash(req.body.password, saltPass);
 
       //Creates a new user
       let user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: securedPass,
       });
-      res.json(user);
-    } catch (err) {
+
+      //Making JWT Token
+      const tokenData = {
+        user: {
+          id: user.id
+        }
+      };
+      let authToken = jwt.sign(tokenData, AUTH_SECRET);
+      //Sending JWT Token to user to authenticate
+      res.json({ authToken });
+
+      // res.json(user);
+    } catch (error) {
       res
         .status(500)
-        .json({ error: "Some error occured", message: err.message });
-      console.error(err.message);
+        .json({ error: "Internal Server Error", message: error.message });
+      console.error(error.message);
     }
   }
 );
+
+//ENDPOINT 2: Post Method for /cloudbook/auth/login. No login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a Valid Email").isEmail(),
+    body("password", "Password cannot be empty").exists(),
+  ],
+  async (req, res) => {
+    //Check Validations and show errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      //Check Email exists in dataBase
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Incorrect Email or Password" });
+      }
+      //Converts pass to hash and compare hash to Check the correct password
+      let passCheck = await bcrypt.compare(password, user.password);
+      if (!passCheck) {
+        return res.status(400).json({ error: "Incorrect Email or Password" });
+      }
+      //If both credentials are correct then sending the payload(tokenData)
+
+      //Making JWT Token
+      const tokenData = {
+        user:{
+          id: user.id
+        }
+      };
+      let authToken = jwt.sign(tokenData, AUTH_SECRET);
+
+      //Sending JWT Token to user to authenticate
+      res.json({ authToken });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Internal Server Error", message: error.message });
+      console.error(error.message);
+    }
+  }
+);
+
+//ENDPOINT 3: Post Method for /cloudbook/auth/getuser. Login required
+router.post("/getuser", fetchUser, async (req, res) => {
+  try {
+    //Fetch user id from data sent by middleware (fetchUser)
+    let userId = req.user.id;
+    //Select user data (except password) fron database using userId
+    const user = await User.findById(userId).select("-password");
+    //Send user received as response
+    res.json(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
+    console.error(error.message);
+  }
+});
 
 module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,135 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const Note = require("../models/Note");
+const fetchUser = require("../middleware/fetchUser");
 
-router.post('/', (req, res)=>{
-    
-    res.json([]);
-})
+//ENDPOINT 1: Get Method for /cloudbook/notes/getallnotes. Login required
+router.get("/getallnotes", fetchUser, async (req, res) => {
+  try {
+    //Finding all notes using the user id (foreign key)
+    const notes = await Note.find({ user: req.user.id });
+    //sending all notes as json response
+    res.json(notes);
+  } catch (error) {
+    //If any error occurs, Bad Request 500 and custom message
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
+    console.error(error.message);
+  }
+});
 
-module.exports = router
+//ENDPOINT 2: Post Method for /cloudbook/notes/addnote. Login required
+router.post(
+  "/addnote",
+  fetchUser,
+  [
+    //Validating notes field using express validator
+    body("title", "Titile should be at least 3 character").isLength({ min: 3 }),
+    body("description", "Description should be at least 5 character").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      //Check Validations and show errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      //Getting notes details using destructuring from request body
+      let { title, description, tag } = req.body;
+      //Creates new note and save in database
+      const note = await Note.create({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      res.json(note);
+    } catch (error) {
+      //If any error occurs, Bad Request 500 and custom message
+      res
+        .status(500)
+        .json({ error: "Internal Server Error", message: error.message });
+      console.error(error.message);
+    }
+  }
+);
+
+//ENDPOINT 3: Patch Method for /cloudbook/notes/updatenote. Login required
+router.patch("/updatenote/:id", fetchUser, async (req, res) => {
+  try {
+    //Get notes details using destructing from request body
+    let { title, description, tag } = req.body;
+    //Create a newNote using the details passed
+    let newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    //Checks if the note exist the one user trying to access
+    let note = await Note.findOne({ _id: req.params.id });
+    if (!note) {
+      return res.status(404).json({ error: "Note not found" });
+    }
+    //Checks if the note's user is same as the logged-in user
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).json({ error: "Not allowed to update this note" });
+    }
+    //Updates the note by finding it's id
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+
+    //Send the updated note as json response
+    res.json(note);
+  } catch (error) {
+    //If any error occurs, Bad Request 500 and custom message
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
+    console.error(error.message);
+  }
+});
+
+//ENDPOINT 4: Delete Method for /cloudbook/notes/deletenote. Login required
+router.delete("/deletenote/:id", fetchUser, async (req, res) => {
+  try {
+
+    //Checks if the note exist the one user trying to delete
+    let note = await Note.findOne({ _id: req.params.id });
+    if (!note) {
+      return res.status(404).json({ error: "Note not found" });
+    }
+    //Checks if the note's user is same as the logged-in user
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).json({ error: "Not allowed to update this note" });
+    }
+    //Deletes the note by finding it's id
+    note = await Note.findByIdAndDelete(req.params.id);
+    //Send success message and deleted note as json response
+    res.json({
+      message: "Success: Note has been deleted",
+      note: note
+    });
+  } catch (error) {
+    //If any error occurs, Bad Request 500 and custom message
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
+    console.error(error.message);
+  }
+});
+
+module.exports = router;
