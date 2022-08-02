4167fbb432199432c806b614289368d93418f28f
codaestroTayyab <tayyab.saeed1@gmail.com>
2022-08-02 06:25:30 +0500

    modalContext - removed

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./contexts/NoteStates";
-import ModalContext from "./contexts/ModalContext";
 
 function App() {
   return (
     <div>
       <NoteState>
-        <ModalContext>
         <Navbar/>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
         </Routes>
-        </ModalContext>
       </NoteState>
     </div>
   );
